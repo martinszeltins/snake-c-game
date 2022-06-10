b95e8a541f1938d4b88a6f437269582d69a31ede
Martins Zeltins <martinszeltins@gmail.com>
2022-06-10 12:46:41 +0300

    Refactoring and cleanup

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@ int main(int argc, char **argv)
 {
     struct snake *snake_game = create_snake_game();
 
-	init_game(snake_game);
+    init_game(snake_game);
     snake_game_run(snake_game);
     destroy_snake_game(snake_game);
 
